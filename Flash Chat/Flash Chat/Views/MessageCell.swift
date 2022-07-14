fd6188773c3dd556c5353534c47040c328fe2919
Cosme Alves <63716324+cosmealvess@users.noreply.github.com>
2022-07-14 18:03:58 +1000

    commit
    commit

--- Flash Chat/Flash Chat/Views/MessageCell.swift
+++ Flash Chat/Flash Chat/Views/MessageCell.swift
@@ -8,16 +8,49 @@
 import UIKit
 
 class MessageCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier:String = "MessageCell"
+    
+    lazy var UserImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview()
+        self.setUpConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubview() {
+        self.contentView.addSubview(self.UserImageView)
+        self.contentView.addSubview(self.nameLabel)
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+        
+            self.UserImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.UserImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.UserImageView.heightAnchor.constraint(equalToConstant: 80),
+            self.UserImageView.widthAnchor.constraint(equalToConstant: 80),
+            
+            self.nameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.nameLabel.leadingAnchor.constraint(equalTo: self.UserImageView.trailingAnchor, constant: 20),
+        ])
     }
 
 }
